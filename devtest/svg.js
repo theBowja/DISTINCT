a0de407398cc8fbeb0bffcca5ad7fb0baf462684
theBowja <ericxin@gmail.com>
2017-05-22 15:08:16 -0400

    media button (play/pause)
    alternates. functionality is broken tho

--- devtest/svg.js
+++ devtest/svg.js
@@ -38,10 +38,10 @@ var SVGGRAPH = (function() {
 	 	.attr("class", "forms")
 		;
 
-	var toolbar = svg.append("g")
+	var toolBox = svg.append("g")
 		.attr("id", "toolbox")
 		.attr("transform", "translate(10,10)");
-	initToolbar(toolbar); // idk conventions for init
+	initToolbar(toolBox); // idk conventions for init
 
 	var simulation = d3.forceSimulation()
 	   	.force("link", d3.forceLink().id(function(d) { return d.name; })) // force link with id specified
@@ -75,64 +75,67 @@ var SVGGRAPH = (function() {
 			.enter().append("line"); 
 	}
 
-	function initToolbar(toolbar) {
+	function initToolbar(toolBox) {
 		// makes a clipping of the outline which we'll use to cut off extraneous background
-		var clip = toolbar.append("defs").append("clipPath")
+		var clip = toolBox.append("defs").append("clipPath")
 			.attr("id", "toolclipBox")
 		clip.append("rect")
 			.attr("id", "toolBox")
 			.attr("width", 24)
-			.attr("height", 72)
-			.attr("rx", 10)
-			.attr("ry", 10)
+			.attr("height", 48)
+			.attr("rx", 2)
+			.attr("ry", 2)
 			.style("fill", "none")
 			.style("stroke", "black")
-			.style("stroke-width", 1);
+			.style("stroke-width", 2);
+
+		toolBox.attr("clip-path", "url(#toolclipBox)");
 
 		// PLAY ICON
-		var playbutton = toolbar.append("g")
+		var mediabutton = toolBox.append("g")
 			.attr("class", "toolbox-button")
 			.attr("transform", "translate(0,0)")
-			.attr("clip-path", "url(#toolclipBox")
-			.on("click", function() {console.log("clicked");})
-			//.on("hover focus", function() {})
-		playbutton.append("rect")
+			.on("click", (function() { // when user clicks this, this function alternates media symbols
+				var play = true;
+				return function() {
+					play = !play;
+					if( play === true) {
+						d3.select("#media-path").attr("d", "M6 19h4V5H6v14zm8-14v14h4V5h-4z"); // pause symbol
+						simulation.restart();
+					} else if( play === false) {
+						d3.select("#media-path").attr("d", "M8 5v14l11-7z"); // play symbol
+						simulation.stop();
+					}
+				};
+			})() );
+		mediabutton.append("rect")
 			.attr("class", "toolbox-box")
 			.attr("width", 24)
-			.attr("height", 24);
-		playbutton.append("path")
-			.attr("id", "toolbox-path")
-			.attr("d", "M8 5v14l11-7z");
-
-			
-		// PAUSE
-		// var pausebutton = toolbar.append("g")
-		// 	.attr("class", "pausebutton")
-		// 	.attr("transform", "translate(10,34)");
-		// pausebutton.append("path")
-		// 	.attr("d", "M0 0h24v24H0z")
-		// 	.attr("fill", "none");
-		// pausebutton.append("path")
-		// 	.attr("d", "M6 19h4V5H6v14zm8-14v14h4V5h-4z");
+			.attr("height", 24)
+			.attr("shape-rendering", "crispEdges");
+		mediabutton.append("path")
+			.attr("id", "media-path")
+			.attr("d", "M6 19h4V5H6v14zm8-14v14h4V5h-4z"); // shows pause button; means simulation is on play
 
 		// CURSOR
-		var cursorbutton = toolbar.append("g")
+		var cursorbutton = toolBox.append("g")
 			.attr("class", "toolbox-button")
 			.attr("transform", "translate(0,24)");
 		cursorbutton.append("rect")
 			.attr("class", "toolbox-box")
 			.attr("width", 24)
 			.attr("height", 24)
+			.attr("shape-rendering", "crispEdges")
 		cursorbutton.append("path")
 			//.attr("d", "M13.64,21.97C13.14,22.21 12.54,22 12.31,21.5L10.13,16.76L7.62,18.78C7.45,18.92 7.24,19 7,19A1,1 0 0,1 6,18V3A1,1 0 0,1 7,2C7.24,2 7.47,2.09 7.64,2.23L7.65,2.22L19.14,11.86C19.57,12.22 19.62,12.85 19.27,13.27C19.12,13.45 18.91,13.57 18.7,13.61L15.54,14.23L17.74,18.96C18,19.46 17.76,20.05 17.26,20.28L13.64,21.97Z")
 			//.attr("d", "M13,6V11H18V7.75L22.25,12L18,16.25V13H13V18H16.25L12,22.25L7.75,18H11V13H6V16.25L1.75,12L6,7.75V11H11V6H7.75L12,1.75L16.25,6H13Z")
 			.attr("d", "M10,2A2,2 0 0,1 12,4V8.5C12,8.5 14,8.25 14,9.25C14,9.25 16,9 16,10C16,10 18,9.75 18,10.75C18,10.75 20,10.5 20,11.5V15C20,16 17,21 17,22H9C9,22 7,15 4,13C4,13 3,7 8,12V4A2,2 0 0,1 10,2Z")
-
 			
 		// shows the actual outline we used
-		toolbar.append("use")
+		toolBox.append("use")
 			.attr("href", "#toolBox")
-			.attr("transform", "translate(0,0)"); // unneeded
+			.attr("transform", "translate(0,0)") // unneeded
+			.style("stroke-width", 1); // actual stroke-width := min(this.stroke-width, clip.stroke-width)
 		}
 
 	function tick() {
@@ -148,7 +151,7 @@ var SVGGRAPH = (function() {
 	}
 
 	function dragstarted(d) {
-		if (!d3.event.active) simulation.alphaTarget(0.3).restart(); // heats the simulation
+		//if (!d3.event.active) simulation.alphaTarget(0.3).restart(); // heats the simulation
 		d.fx = d.x;
 		d.fy = d.y;
 	}
@@ -159,12 +162,12 @@ var SVGGRAPH = (function() {
 	}
 
 	function dragended(d) {
-		if (!d3.event.active) simulation.alphaTarget(0);
+		//if (!d3.event.active) simulation.alphaTarget(0);
 		d.fx = null;
 		d.fy = null;
 	}
 
-	// TODO: batch up DOM modifications and do them all at once
+	// TODO: organize all functions pertaining to options panel into another file
 	function createNodeOptionsPanel(d) {
 		console.log(d, d3.select(this).datum());
 		var circleNode = this;
